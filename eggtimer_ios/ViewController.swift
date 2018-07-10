4fb470c83cae8d5479a47d3da25e89b123d044fe
許均銘 <tel886912338986@gmail.com>
2018-07-10 14:43:27 +0800

    Initial Commit

---
+++ eggtimer_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggtimer_ios
+//
+//  Created by CHUNMING HSU on 2018/7/10.
+//  Copyright © 2018年 CHUNMING HSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
